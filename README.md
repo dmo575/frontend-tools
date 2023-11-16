f7df580638971650506728d94674df10fa49be25
dmo575 <dmo5@protonmail.com>
2023-11-16 11:52:55 -0500

    Added missing files to last update

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # About
 
-This is a place where I will be adding different  components and front end features that I build as I practice, like a responsive navbar, an image slide-show component... things like that.
+This is a place where I will be adding different components and front end features that I build as I practice, like a responsive navbar, an image slide-show component... things like that.
 
 ## What am I using
 - Vite as the build tool
 - React
 - Plain JS/CSS/HTML
 
-## How to see all the components
+## responsive-navbar
+Is a component that lets you create a responsive nav-bar.
 
-To check out any component, you just have to go into Sandbox.jsx and add in there the component you want to see. Some components need parameters, so please make sure to checkout the component first.
-
-I will leave the components commented out once I finish adding them all in case you want to quickly try them.
+## directory sctructure - tree
--- src/Sandbox.jsx
+++ src/Sandbox.jsx
@@ -3,9 +3,9 @@ import bp from "./responsive.js";
 import logo from "./assets/react.svg";
 
 // components
-import Navbar from './responsive-navbar/Navbar'
+import Navbar from './responsive-navbar/Navbar.jsx'
 
-
+// navbar data
 const  child1a = {title: "Child1_A", href: "www.google.com", target: "_blank"};
 const  child1b = {title: "Child1_B", href: "www.youtube.com", target: "_blank"};
 const  child1a1 = {title: "Child1_A1", href: "www.google.com", target: "_blank"};
@@ -14,30 +14,13 @@ const  child1a3 = {title: "Child1_A3", href: "www.google.com", target: "_blank"}
 const  child1a4 = {title: "Child1_A4", href: "www.google.com", target: "_blank"};
 const  child1a5 = {title: "Child1_A5", children: [child1a, child1a1, child1a2,child1a3,child1a4]};
 const  child1c = {title: "Child1_C", children: [child1a, child1a5, child1a1, child1a2, child1a3, child1a4]};
-
-
-// dropdown modes:
-// left - opens the dropdown to the left of the button
-// right - opens the dropdown to the right of the button
-// top - opens the dropdown at the top of the button
-// bot - opens the dropdown at the bottom of the button
-
-// align modes:
-// center - dropdown is aligned at the center
-// left - dropdown is aligned
-
-
-// navbar data
 const buttons = [
     {title: "Button1", children: [child1a, child1c, child1b], mode_default: "left", align_default: "center", mode_sm: "left", align_sm: "center"},
     {title: "Button2", href: "www.google.com", target: "_blank"},
     {title: "Button3", href: "www.youtube.com", target: "_blank"},
 ];
 
-/**
- * given buttons, spawn
- */
-
+// contains current breakpoint as the state "currBp"
 export const BreakpointContext = createContext();
 
 function Sandbox() {
@@ -69,7 +52,7 @@ function Sandbox() {
 
     return(
         <BreakpointContext.Provider value={currBp}>
-        <Navbar buttons={buttons} logo={logo} menuButtonLogo={logo}/>
+        <Navbar logo={logo} menuButtonLogo={logo}/>
         </BreakpointContext.Provider>
     );
 }
