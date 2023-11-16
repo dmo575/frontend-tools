863920c8221319e8eec315d5288e6b5fecaaec21
dmo575 <dmo5@protonmail.com>
2023-11-16 00:05:17 -0500

    working on dropdown, refactoring and adding new features

--- src/responsive-navbar/DropdownButton.jsx
+++ src/responsive-navbar/DropdownButton.jsx
@@ -10,14 +10,33 @@ const anchorIcon = "https://api.iconify.design/gg:external.svg";
 const folderIcon = "https://api.iconify.design/material-symbols:arrow-left-rounded.svg";
 const folderIconDown = "https://api.iconify.design/material-symbols:arrow-drop-down-rounded.svg";
 
-function DropdownButton({title, children, dropdownClass, zIndex, customStyles}) {
+function DropdownButton({title, children, dropdownPosClass, zIndex, customStyles}) {
+
+    // handles showing folder content on click, if selected
+    const showFolderOnClick = (event) => {
+    
+        const dropdown = event.target.parentNode.querySelector(".dropdown");
+
+        dropdown.style.display = "block";
+
+        const clear = () => {
+
+            dropdown.style.display = "";
+            dropdown.removeEventListener("mouseout", clear);
+            event.target.removeEventListener("mouseout", clear);
+        }
+
+        dropdown.addEventListener("mouseout", clear);
+        event.target.addEventListener("mouseout", clear);
+    };
+
 
     return(
-        <div className="button-container">
-            <button className={`navbar-button ${customStyles}`}><img className="navbar-button-img" src={children?.length > 0 ? folderIcon : anchorIcon } alt="" />{title}</button>
+        <div className={`dd-item-cont ${customStyles["container"]}`}>
+            <button onClick={showFolderOnClick} className={`dd-button ${customStyles["button"]}`}><img src={children?.length > 0 ? folderIcon : anchorIcon } alt="" />{title}</button>
             {
                 children?.length > 0 && (
-                <div className={`dropdown ${dropdownClass}`} style={{zIndex: zIndex}}>
+                <div className={`dropdown ${dropdownPosClass} ${customStyles["dropdown"]}`} style={{zIndex: zIndex}}>
                     { children?.map(el => el) }
                 </div>)
             }
--- src/responsive-navbar/Navbar.jsx
+++ src/responsive-navbar/Navbar.jsx
@@ -1,11 +1,62 @@
 import bp from "../responsive.js";
-import "./styles.css";
+import "./styles_navbar.css";
 
 import { useEffect, useContext } from "react";
 import { BreakpointContext } from "../Sandbox.jsx";
 import DropdownButton from "./DropdownButton.jsx";
 
-function Navbar({buttons, logo, menuButtonLogo}) {
+// TODO, EXPERIMENT WITH CHANGING POSITION AND HAVING A FIXED DROPDOWN SIZE, ONE BACK BUTTON
+
+
+const root_folder1 = {
+    title: "First folder",
+}
+const root_folder2 = {
+    title: "Second folder"
+}
+
+const someAnchor1 = {
+    title: "Some anchor"
+}
+const someAnchor2 = {
+    title: "Another one"
+}
+const someAnchor3 = {
+    title: "Hello"
+}
+const someAnchor4 = {
+    title: "Hello23"
+}
+const someAnchor5 = {
+    title: "Hello44"
+}
+
+const lvl1_folder1 = {
+    title: "folder_11"
+}
+const lvl1_folder2 = {
+    title: "folder_12"
+}
+
+const lvl2_folder1 = {
+    title: "folder_21"
+}
+
+const lvl2_folder2 = {
+    title: "folder_22"
+}
+
+root_folder1.children = [someAnchor1, someAnchor2, someAnchor3, lvl1_folder1, lvl1_folder2]
+lvl1_folder1.children = [someAnchor1, lvl2_folder1, someAnchor2, someAnchor3, lvl2_folder2, someAnchor4]
+lvl1_folder2.children = [someAnchor5, someAnchor1, someAnchor4, someAnchor2]
+lvl2_folder1.children = [someAnchor3]
+lvl2_folder2.children = [];
+
+root_folder2.children = [someAnchor1, someAnchor2, someAnchor3];
+
+
+
+function Navbar({buttons=[root_folder1, root_folder2], logo, menuButtonLogo}) {
 
     const currBp = useContext(BreakpointContext);
 
@@ -16,13 +67,14 @@ function Navbar({buttons, logo, menuButtonLogo}) {
      * It allows you pass in custom styles for every folder.
      * 
      * Detailed:
-     * root: 
+     * root:
      *      the first parent/s button/s
      *      the object passed must contain the following elements:
      *          -children - an array containing children elements, if any
      *              - an element with this property will be treated as a folder, a button
      *          - title - the name to give to the element/folder/link
-     *          - icon (optional) - an icon to be displayed at the left of the item
+     *          - icons (optional) - an object containing icons to use:
+     *              - {root_folder: "src", root_anchor: "src", folder: "src", anchor: "src"}
      * levelStyles:
      *      accepts an array of the object:
      *      - {container: "container styles", "button": "button styles", dropdown: "dropdown styles"}
@@ -31,15 +83,21 @@ function Navbar({buttons, logo, menuButtonLogo}) {
      *  zIndexStart:
      *      - the z index value you want to start with for the main dropdown, each subsequent dropdown will be rendered
      *        at an higher index than its parent.
+     * _recursionCap:
+     *      - If the function calls itself more than the specified count, it will throw an error.
+     *      - this detects closed dependency loops between the element's relationship structure
      * _currLevel:
-     *      - used internally by the recursive function, you should not edit this variable as indicated by its lowercase
-     *        naming.
+     *      - used internally by the recursive function, you should not edit this variable as indicated 
+     *        by its lowercase naming.
      *      - it keeps track of how deep the function is on its recursive calling. Used to figure out what 
      *        z index, custom styles and key to provide to the DropdownButton component.
+     * 
      */
-    const spawnButtons_stairs = (root, levelStyles=["", "test", "test2", "test3"], zIndexStart=0, _currLevel=0) => {
+
+    const spawnButtons_stairs = (root, levelStyles=["", {container: "test", dropdown: "test"}, {button: "test2", dropdown: "test22",container: ""}], zIndexStart=0, _recursionCap=200, _currLevel=0) => {
 
         if(!root) return null;
+        if(_currLevel > _recursionCap) {throw new Error("recursion cap exeeded. You might have a closed dependency loop.")};
 
         // The first dropdown should appear below to the left of the parent, the other ones at the left of the parent
         // as described in the css classes.
@@ -47,7 +105,7 @@ function Navbar({buttons, logo, menuButtonLogo}) {
 
         // is the root is headless, return an array
         if(root.length > 1) {
-            return root.map(el => spawnButtons_stairs(el, levelStyles ,zIndexStart, _currLevel));
+            return root.map(el => spawnButtons_stairs(el, levelStyles ,zIndexStart, _recursionCap, _currLevel));
         }
 
         // used to store children
@@ -59,7 +117,7 @@ function Navbar({buttons, logo, menuButtonLogo}) {
             // does that child has children?
             if(root.children[i].children) {
                 // recursively call spawnButtons on child and add its return to array
-                childButtons.push(spawnButtons_stairs(root.children[i], levelStyles, zIndexStart, _currLevel+1));
+                childButtons.push(spawnButtons_stairs(root.children[i], levelStyles, zIndexStart, _recursionCap, _currLevel+1));
             }
             // child does NOT has any children (this means it's an anchor)
             else {
@@ -67,9 +125,9 @@ function Navbar({buttons, logo, menuButtonLogo}) {
                 childButtons.push( <DropdownButton 
                     key={`${root.children[i].title}-${_currLevel}`} 
                     title={root.children[i].title} 
-                    dropdownClass={dropdownClass} 
+                    dropdownPosClass={dropdownClass} 
                     zIndex={zIndexStart + _currLevel} 
-                    customStyles={levelStyles[_currLevel+1]}/> 
+                    customStyles={levelStyles[_currLevel]}/> 
                 );
             }
         }
@@ -79,7 +137,7 @@ function Navbar({buttons, logo, menuButtonLogo}) {
             <DropdownButton 
             key={`${root.title}-${_currLevel}`} 
             title={root.title} 
-            dropdownClass={dropdownClass} 
+            dropdownPosClass={dropdownClass} 
             zIndex={zIndexStart + _currLevel} 
             customStyles={levelStyles[_currLevel]}>
                 {childButtons.map(el => el)}
@@ -98,12 +156,12 @@ function Navbar({buttons, logo, menuButtonLogo}) {
     };
 
     return(
-        <div className="navbar">
-            <div className="logo-div">
+        <div className="navbar-cont">
+            <div className="navbar-logo-cont">
                 <img className="navbar-logo" src={logo} alt="logo image"/>
             </div>
-            <div className="buttons-div">
-                { currBp > bp.xs ? spawnButtons_stairs(buttons,["", "test", "test2", "test3"],2) : spawnButtons_slide(buttons)}
+            <div className="navbar-buttons-cont">
+                { currBp > bp.xs ? spawnButtons_stairs(buttons) : spawnButtons_slide(buttons)}
             </div>
         </div>
     );
